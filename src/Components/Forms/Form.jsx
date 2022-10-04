4869c89103a784f31ed886c6d853edb273f1e54d
david_semen <dsemenenko7@gmail.com>
2022-10-04 17:54:30 +0400

    update form.jsx

--- src/Components/Forms/Form.jsx
+++ src/Components/Forms/Form.jsx
@@ -30,8 +30,9 @@ const Form = (props) => {
       } = useForm();
     
     // register = JSON.stringify([data])
-
-    
+      
+    const[message, setMessage] = useState('');
+    const [lang, setLang] = useState(null);
 
     //Post value 
     const [title, setTitle] = useState('');
@@ -88,23 +89,24 @@ const Form = (props) => {
         // this.fields.concat('')
         
         
-        Axios.post('https://crm.axcap.ae/rest/1/y9x9q1wmj1mwq5bu/crm.lead.update/', { 
+        Axios.post('https://crmdev2.axcap.ae/rest/1/y9x9q1wmj1mwq5bu/crm.lead.update/', { 
             "id": props.restid,
             fields,
             "params": {
                 "REGISTER_SONET_EVENT": "Y"
             }
         }).then(fields => console.log('Posting data', fields)).catch(err => console.log(err ));
-        reset()
         
-        Axios.post('https://crm.axcap.ae/rest/1/y9x9q1wmj1mwq5bu/crm.timeline.comment.add/', { 
+        
+        Axios.post('https://crmdev2.axcap.ae/rest/1/y9x9q1wmj1mwq5bu/crm.timeline.comment.add/', { 
             "fields":{
                 "ENTITY_ID": props.restid,
                 "ENTITY_TYPE": "lead",
                 "COMMENT": fields.COMMENT
             }
         }).then(fields => console.log('Posting data', fields)).catch(err => console.log(err ));
-
+        setMessage(`-- Lead has been update successful --`);
+        reset();
       };
 
 
@@ -117,6 +119,47 @@ const Form = (props) => {
         return yyyy + "-" + mm + "-" + dd;
     };
     
+    function getLangDiv(){
+        switch(lang){
+            case "IN_PROCESS": 
+                return <div className="form-floating">
+                        <select 
+                            className="form-label form-control form-select is-invalid"
+                            {...register("UF_CRM_1553506485[0]", {required: true})}
+                        >
+                            <option value="">--Add  language--</option>
+                            <option value="2925">English</option>
+                            <option value="2927">Arabic</option>
+                            <option value="2926">Russian</option>
+                            <option value="2928">Urdu</option>
+                            <option value="2931">German</option>
+                            <option value="2932">French</option>
+                            <option value="4227">Hindi</option>
+                            <option value="5085">Farsi</option>
+                            <option value="5943">Italian</option>
+                            <option value="11420">Portuguese</option>
+                            <option value="6834">Spanish</option>
+                            <option value="6943">Kazakh</option>
+                            <option value="6944">Uzbek</option>
+                            <option value="8142">Romanian</option>
+                            <option value="10523">Ukranian</option>
+                            <option value="10524">Belorusian</option>
+                            <option value="11382">Azerbaijan</option>
+                            <option value="11383">Chechen</option>
+                            <option value="12058">Dutch</option>
+                            <option value="12082">Hebrew</option>
+                            <option value="12407">Czech</option>
+                            <option value="16871">Chinese</option>
+                            <option value="16915">Polish</option>
+                        </select>
+                        <span class="invalid-feedback">This field is required</span>
+                        <label for="floatingSelect">Update LEAD language</label>
+                    </div>
+                default: 
+                return null;
+        }
+    }
+
     return(
         <div className="card">
             <div className="card-body">
@@ -124,6 +167,9 @@ const Form = (props) => {
                 <form className="leadstatus" onSubmit={handleSubmit(onSubmit)}>
                     <div className="">
                         <select  
+                            onClick={(event) => {
+                                setLang(event.target.value)
+                            }}
                             className={`p-2 form-label form-control form-select ${errors.STATUS_ID && "is-invalid"}`} 
                             {...register("STATUS_ID", { required: 'Gender is required' })} 
                         >
@@ -134,7 +180,7 @@ const Form = (props) => {
                         {errors.STATUS_ID && <span class="invalid-feedback">This field is required</span>}
                     </div>
 
-                    <div className="form-floating">
+                    {/* <div className="form-floating">
                         <select 
                             className="form-label form-control form-select"
                             {...register("UF_CRM_1553506485[0]", {required: false})}
@@ -165,7 +211,10 @@ const Form = (props) => {
                             <option value="16915">Polish</option>
                         </select>
                         <label for="floatingSelect">Update LEAD language</label>
-                    </div>
+                    </div> */}
+
+                    {getLangDiv()}
+                    
 
                     <div className="mb-3 ">
                         <label className="form-label text-white">Date/Time</label>
@@ -191,6 +240,13 @@ const Form = (props) => {
                             {...register("COMMENT", {required: true})}
                         />
                         {errors.COMMENT && <span className="invalid-feedback">There should be a comment</span>}
+                    </div>
+
+                    
+                    <div class="bg-success text-white">
+                        <div class="text-center">
+                            {message}
+                        </div>
                     </div>
 
                     {/* <div className="form-floating">
